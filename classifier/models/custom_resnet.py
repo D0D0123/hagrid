505463cf21019c8f8cf58366b00c0ddfed5781ec
Soura Mandal <souramandal@pop-os.localdomain>
2022-07-20 23:43:03 +1000

    changed residual connections from 2, 2, 2, 2 to 1, 1, 1, 1

--- classifier/models/custom_resnet.py
+++ classifier/models/custom_resnet.py
@@ -8,12 +8,6 @@ import torch
 import torch.nn as nn
 from torch import Tensor
 
-
-__all__ = [
-    "CustomResNet",
-    "resnet18",
-]
-
 def conv3x3(in_planes: int, out_planes: int, stride: int = 1, groups: int = 1, dilation: int = 1) -> nn.Conv2d:
     """3x3 convolution with padding"""
     return nn.Conv2d(
@@ -136,74 +130,15 @@ class CustomBlock(nn.Module):
 
 #####################################
 
-class Bottleneck(nn.Module):
-    # Bottleneck in torchvision places the stride for downsampling at 3x3 convolution(self.conv2)
-    # while original implementation places the stride at the first 1x1 convolution(self.conv1)
-    # according to "Deep residual learning for image recognition"https://arxiv.org/abs/1512.03385.
-    # This variant is also known as CustomResNet V1.5 and improves accuracy according to
-    # https://ngc.nvidia.com/catalog/model-scripts/nvidia:resnet_50_v1_5_for_pytorch.
-
-    expansion: int = 4
-
-    def __init__(
-        self,
-        inplanes: int,
-        planes: int,
-        stride: int = 1,
-        downsample: Optional[nn.Module] = None,
-        groups: int = 1,
-        base_width: int = 64,
-        dilation: int = 1,
-        norm_layer: Optional[Callable[..., nn.Module]] = None,
-    ) -> None:
-        super().__init__()
-        if norm_layer is None:
-            norm_layer = nn.BatchNorm2d
-        width = int(planes * (base_width / 64.0)) * groups
-        # Both self.conv2 and self.downsample layers downsample the input when stride != 1
-        self.conv1 = conv1x1(inplanes, width)
-        self.bn1 = norm_layer(width)
-        self.conv2 = conv3x3(width, width, stride, groups, dilation)
-        self.bn2 = norm_layer(width)
-        self.conv3 = conv1x1(width, planes * self.expansion)
-        self.bn3 = norm_layer(planes * self.expansion)
-        self.relu = nn.ReLU(inplace=True)
-        self.downsample = downsample
-        self.stride = stride
-
-    def forward(self, x: Tensor) -> Tensor:
-        identity = x
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-        out = self.relu(out)
-
-        out = self.conv3(out)
-        out = self.bn3(out)
-
-        if self.downsample is not None:
-            identity = self.downsample(x)
-
-        out += identity
-        out = self.relu(out)
-
-        return out
-
-
 class CustomResNet(nn.Module):
     def __init__(
         self,
-        block: Type[Union[CustomBlock, Bottleneck]],
+        block: CustomBlock,
         layers: List[int],
         num_classes: int = 1000,
         zero_init_residual: bool = False,
         groups: int = 1,
         width_per_group: int = 64,
-        replace_stride_with_dilation: Optional[List[bool]] = None,
         norm_layer: Optional[Callable[..., nn.Module]] = None,
     ) -> None:
         super().__init__()
@@ -213,15 +148,6 @@ class CustomResNet(nn.Module):
 
         self.in_channels = 64
         self.dilation = 1
-        if replace_stride_with_dilation is None:
-            # each element in the tuple indicates if we should replace
-            # the 2x2 stride with a dilated convolution instead
-            replace_stride_with_dilation = [False, False, False]
-        if len(replace_stride_with_dilation) != 3:
-            raise ValueError(
-                "replace_stride_with_dilation should be None "
-                f"or a 3-element tuple, got {replace_stride_with_dilation}"
-            )
         self.groups = groups
         self.base_width = width_per_group
         self.conv1 = nn.Conv2d(3, self.in_channels, kernel_size=7, stride=2, padding=3, bias=False)
@@ -247,14 +173,11 @@ class CustomResNet(nn.Module):
         # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
         if zero_init_residual:
             for m in self.modules():
-                if isinstance(m, Bottleneck):
-                    nn.init.constant_(m.bn3.weight, 0)  # type: ignore[arg-type]
-                elif isinstance(m, CustomBlock):
-                    nn.init.constant_(m.bn2.weight, 0)  # type: ignore[arg-type]
+                nn.init.constant_(m.bn2.weight, 0)  # type: ignore[arg-type]
 
     def _make_layer(
         self,
-        block: Type[Union[CustomBlock, Bottleneck]],
+        block: CustomBlock,
         planes: int,
         blocks: int,
         stride: int = 1,
@@ -313,8 +236,9 @@ class CustomResNet(nn.Module):
                 self.in_channels, 
                 intermediate_channels, 
                 stride, 
-                identity_downsample)
+                identity_downsample
             )
+        )
         
         self.in_channels = intermediate_channels # 256
         for i in range(num_residual_blocks - 1):
@@ -350,17 +274,15 @@ class CustomResNet(nn.Module):
 
 
 def _resnet(
-    arch: str,
-    block: Type[Union[CustomBlock, Bottleneck]],
+    block: CustomBlock,
     layers: List[int],
-    progress: bool,
     **kwargs: Any,
 ) -> CustomResNet:
     model = CustomResNet(block, layers, **kwargs)
     return model
 
 
-def resnet18(pretrained: bool = False, progress: bool = True, **kwargs: Any) -> CustomResNet:
+def resnet18(progress: bool = True, **kwargs: Any) -> CustomResNet:
     r"""CustomResNet-18 model from
     `"Deep Residual Learning for Image Recognition" <https://arxiv.org/pdf/1512.03385.pdf>`_.
 
@@ -368,4 +290,4 @@ def resnet18(pretrained: bool = False, progress: bool = True, **kwargs: Any) -> 
         pretrained (bool): If True, returns a model pre-trained on ImageNet
         progress (bool): If True, displays a progress bar of the download to stderr
     """
-    return _resnet("resnet18", CustomBlock, [2, 2, 2, 2], progress, **kwargs)
+    return _resnet(CustomBlock, [1, 1, 1, 1], **kwargs)
