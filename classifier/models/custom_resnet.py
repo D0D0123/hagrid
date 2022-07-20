2fd0e3aa489d8252b0f7c70f95b11b1a9a9a9817
Soura Mandal <souramandal@pop-os.localdomain>
2022-07-21 00:00:54 +1000

    unnecessary code stripped

--- classifier/models/custom_resnet.py
+++ classifier/models/custom_resnet.py
@@ -2,80 +2,11 @@
 # perceptron-posse
 
 
-from typing import Type, Any, Callable, Union, List, Optional
+from typing import Type, Any, Callable, List, Optional
 
 import torch
 import torch.nn as nn
 from torch import Tensor
-
-def conv3x3(in_planes: int, out_planes: int, stride: int = 1, groups: int = 1, dilation: int = 1) -> nn.Conv2d:
-    """3x3 convolution with padding"""
-    return nn.Conv2d(
-        in_planes,
-        out_planes,
-        kernel_size=3,
-        stride=stride,
-        padding=dilation,
-        groups=groups,
-        bias=False,
-        dilation=dilation,
-    )
-
-
-def conv1x1(in_planes: int, out_planes: int, stride: int = 1) -> nn.Conv2d:
-    """1x1 convolution"""
-    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
-
-
-class BasicBlock(nn.Module):
-    expansion: int = 1
-
-    def __init__(
-        self,
-        inplanes: int,
-        planes: int,
-        stride: int = 1,
-        downsample: Optional[nn.Module] = None,
-        groups: int = 1,
-        base_width: int = 64,
-        dilation: int = 1,
-        norm_layer: Optional[Callable[..., nn.Module]] = None,
-    ) -> None:
-        super().__init__()
-        if norm_layer is None:
-            norm_layer = nn.BatchNorm2d
-        if groups != 1 or base_width != 64:
-            raise ValueError("BasicBlock only supports groups=1 and base_width=64")
-        if dilation > 1:
-            raise NotImplementedError("Dilation > 1 not supported in BasicBlock")
-        # Both self.conv1 and self.downsample layers downsample the input when stride != 1
-        self.conv1 = conv3x3(inplanes, planes, stride)
-        self.bn1 = norm_layer(planes)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3(planes, planes)
-        self.bn2 = norm_layer(planes)
-        self.downsample = downsample
-        self.stride = stride
-
-    def forward(self, x: Tensor) -> Tensor:
-        identity = x
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-
-        if self.downsample is not None:
-            identity = self.downsample(x)
-
-        out += identity
-        out = self.relu(out)
-
-        return out
-
-####################################
 
 class CustomBlock(nn.Module):
     """
@@ -83,18 +14,11 @@ class CustomBlock(nn.Module):
     - Consists of two convolutional layers with 2-d batch normalization between followed by a dropout layer and 
     - ReLU activation function
     """
-
-    expansion: int = 1
-
     def __init__(self, 
                 in_channels, 
                 out_channels, 
                 stride = 1,
                 identity_downsample: Optional[nn.Module] = None,
-                groups: int = 1,
-                base_width: int = 64,
-                dilation: int = 1,
-                norm_layer: Optional[Callable[..., nn.Module]] = None,
                 ):
         super(CustomBlock, self).__init__()
         self.conv2 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1)
@@ -136,22 +60,12 @@ class CustomResNet(nn.Module):
         block: CustomBlock,
         layers: List[int],
         num_classes: int = 1000,
-        zero_init_residual: bool = False,
-        groups: int = 1,
-        width_per_group: int = 64,
-        norm_layer: Optional[Callable[..., nn.Module]] = None,
     ) -> None:
         super().__init__()
-        if norm_layer is None:
-            norm_layer = nn.BatchNorm2d
-        self._norm_layer = norm_layer
 
         self.in_channels = 64
-        self.dilation = 1
-        self.groups = groups
-        self.base_width = width_per_group
         self.conv1 = nn.Conv2d(3, self.in_channels, kernel_size=7, stride=2, padding=3, bias=False)
-        self.bn1 = norm_layer(self.in_channels)
+        self.bn1 = nn.BatchNorm2d(self.in_channels)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         self.layer1 = self._custom_make_layers(block, 64, layers[0], stride=1)
@@ -159,7 +73,7 @@ class CustomResNet(nn.Module):
         self.layer3 = self._custom_make_layers(block, 256, layers[2], stride=2)
         self.layer4 = self._custom_make_layers(block, 512, layers[3], stride=2)
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(512 * block.expansion, num_classes)
+        self.fc = nn.Linear(512, num_classes)
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
@@ -167,54 +81,6 @@ class CustomResNet(nn.Module):
             elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
                 nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.bias, 0)
-
-        # Zero-initialize the last BN in each residual branch,
-        # so that the residual branch starts with zeros, and each residual block behaves like an identity.
-        # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
-        if zero_init_residual:
-            for m in self.modules():
-                nn.init.constant_(m.bn2.weight, 0)  # type: ignore[arg-type]
-
-    def _make_layer(
-        self,
-        block: CustomBlock,
-        planes: int,
-        blocks: int,
-        stride: int = 1,
-        dilate: bool = False,
-    ) -> nn.Sequential:
-        norm_layer = self._norm_layer
-        downsample = None
-        previous_dilation = self.dilation
-        if dilate:
-            self.dilation *= stride
-            stride = 1
-        if stride != 1 or self.in_channels != planes * block.expansion:
-            downsample = nn.Sequential(
-                conv1x1(self.in_channels, planes * block.expansion, stride),
-                norm_layer(planes * block.expansion),
-            )
-
-        layers = []
-        layers.append(
-            block(
-                self.in_channels, planes, stride, downsample, self.groups, self.base_width, previous_dilation, norm_layer
-            )
-        )
-        self.in_channels = planes * block.expansion
-        for _ in range(1, blocks):
-            layers.append(
-                block(
-                    self.in_channels,
-                    planes,
-                    groups=self.groups,
-                    base_width=self.base_width,
-                    dilation=self.dilation,
-                    norm_layer=norm_layer,
-                )
-            )
-
-        return nn.Sequential(*layers)
     
     def _custom_make_layers(
                     self, 
@@ -281,6 +147,7 @@ def _resnet(
     model = CustomResNet(block, layers, **kwargs)
     return model
 
+### duplicate and customise to make custom resnet classes - perceptron-posse ###
 
 def resnet18(progress: bool = True, **kwargs: Any) -> CustomResNet:
     r"""CustomResNet-18 model from
